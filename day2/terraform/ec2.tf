7daa3123ceb15f0a1b732dfe07c94e555b91cbd7
Mohamed7Khalifa <MHMD7Khalifa@gmail.com>
2023-02-15 18:12:38 +0200

    update terraform

--- day2/terraform/ec2.tf
+++ day2/terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "jump_host" {
  ami           = "ami-0557a15b87f6559cf"
- instance_type = "t3.micro"
+ instance_type = "t3.xlarge"
  associate_public_ip_address = true
  subnet_id = aws_subnet.public-az2.id
  vpc_security_group_ids = [aws_security_group.public_sg.id]
@@ -14,7 +14,7 @@ resource "aws_instance" "jump_host" {
 
 resource "aws_instance" "private-vm-az1" {
  ami           = "ami-0557a15b87f6559cf"
-  instance_type = "t3.micro"
+  instance_type = "t3.xlarge"
   associate_public_ip_address = false
   subnet_id = aws_subnet.private-az1.id
   vpc_security_group_ids = [aws_security_group.public_sg.id]
@@ -28,7 +28,7 @@ resource "aws_instance" "private-vm-az1" {
 
 resource "aws_instance" "private-vm-az2" {
  ami           = "ami-0557a15b87f6559cf"
-  instance_type = "t3.micro"
+  instance_type = "t3.xlarge"
   associate_public_ip_address = false
   subnet_id = aws_subnet.private-az2.id
   vpc_security_group_ids = [aws_security_group.public_sg.id]
